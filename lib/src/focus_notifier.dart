53535128e3a3b577edeee53412326f4f4b7f6cdd
Algure <ajirie98@gmail.com>
2022-09-26 00:19:08 +0100

    Documented focus_notifier.dart.

--- lib/src/focus_notifier.dart
+++ lib/src/focus_notifier.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// An [InheritedNotifier] which broadcasts the [isPointed] value to [FocusPointerDetector] widgets further down the widget tree contained in
+/// the [child] parameter.
+/// The [FocusNotifierWidget] widget is an internal widget of this package and rebuilds the widget tree whenever [updateShouldNotify]
+/// is called to ensure proper behaviour of the package.
+///
 class FocusNotifierWidget extends InheritedNotifier{
 
   final bool isPointed;
